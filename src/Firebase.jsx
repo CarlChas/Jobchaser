c1b225a8c787011bc7efd2620cc8f76db9ae0fb8
Carl <carl.mikaelsson@chasacademy.se>
2024-05-18 11:09:48 +0200

    added: firebase stuff

---
+++ src/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzNqu1QI3JPrPhgXOwlBPy8y0aJQ0gixk",
+  authDomain: "jobchaser-54e94.firebaseapp.com",
+  projectId: "jobchaser-54e94",
+  storageBucket: "jobchaser-54e94.appspot.com",
+  messagingSenderId: "481847741080",
+  appId: "1:481847741080:web:0ee17462dcb8db5ffc9eed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
