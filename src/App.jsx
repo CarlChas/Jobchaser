11d4fd1421ff38a08f38864de60230190ca681bd
Carl <carl.mikaelsson@chasacademy.se>
2024-05-19 20:33:13 +0200

    added: Navigation concept

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,47 @@ import Dashboard from './components/Dashboard.jsx'
 import SignInPage from './components/SignInPage.jsx'
 import SignUpPage from './components/SignUpPage.jsx'
 import HomePage from './components/HomePage.jsx'
+import { AuthProvider, useAuth } from './AuthContext'
+import Navigation from './Navigation'
 
 function ProtectedRoute() {
-  const isAuthenticated = false
+  const { currentUser } = useAuth();
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/signin" replace />
+  return currentUser ? <Outlet /> : <Navigate to="/signin" replace />
 }
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/signin'>Sign In</Link>
-            </li>
-            <li>
-              <Link to='/signup'>Sign Up</Link>
-            </li>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            {/* <button>Sign Out</button> */}
-          </ul>
-        </nav>
-      </header>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signin' element={<SignInPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/dashboard' element={<ProtectedRoute />}>
-          <Route path='/dashboard' element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-
+    <AuthProvider>
+      <BrowserRouter>
+        <header>
+          <Navigation />
+          <nav>
+            <ul>
+              <li>
+                <Link to='/signin'>Sign In</Link>
+              </li>
+              <li>
+                <Link to='/signup'>Sign Up</Link>
+              </li>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              {/* <button>Sign Out</button> */}
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/signin' element={<SignInPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/dashboard' element={<ProtectedRoute />}>
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
