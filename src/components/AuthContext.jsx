a3ab11f2047a7d8995388b238de680286e2b833b
Carl <carl.mikaelsson@chasacademy.se>
2024-05-19 20:46:33 +0200

    added: user acc context file

---
+++ src/components/AuthContext.jsx
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useEffect, useState } from "react"
+import { MyAuth } from "./Firebase"
+import { onAuthStateChanged, signOut } from "firebase/auth"
+
+const AuthContext = createContext()
+
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null)
+
+    useEffect(() => {
+        const unSub = onAuthStateChanged(MyAuth, (user) => {
+            setCurrentUser(user)
+        })
+        return unSub
+    }, [])
+
+    const logOut = () => {
+        signOut(MyAuth)
+    }
+
+    const myVal = {
+        currentUser,
+        logOut,
+    }
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
