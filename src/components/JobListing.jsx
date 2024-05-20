f1ddceedf19754770b0e44a43db85215e0ca6365
Carl <carl.mikaelsson@chasacademy.se>
2024-05-20 18:15:05 +0200

    added: jobListing categories

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function JobListing({ job }) {
+/* function JobListing({ job }) {
   return (
     <article className='jobListing'>
       <img id='jobLogo' src={`${job.logo}`} alt={job.company} />
@@ -13,6 +13,28 @@ function JobListing({ job }) {
         <article key={index} className='myTags'>{tool}</article>
       ))}
       <article className='myFavorite'> Star </article>
+    </article>
+  )
+}
+*/
+
+function JobListing({ job }) {
+  return (
+    <article className='jobListing'>
+      <img id='jobLogo' src={job.logo} alt={`${job.company} logo`} />
+      <div className='jobDetails'>
+        <h2 className='jobPosition'>{job.position}</h2>
+        <p className='jobLevel'>{job.level}</p>
+      </div>
+      <div className='jobTags'>
+        {job.languages.length > 0 && job.languages.map((language, index) => (
+          <span key={index} className='myTags'>{language}</span>
+        ))}
+        {job.tools.length > 0 && job.tools.map((tool, index) => (
+          <span key={index} className='myTags'>{tool}</span>
+        ))}
+      </div>
+      <button className='myFavorite'>Star</button>
     </article>
   )
 }
